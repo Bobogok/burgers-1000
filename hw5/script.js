89bf3a10fca94de65407f1296e30db174770a818
Ivan M <40141225+Bobogok@users.noreply.github.com>
2021-03-30 11:50:50 +0300

    update script

--- hw5/script.js
+++ hw5/script.js
@@ -1,71 +1,3 @@
-// function Image(pic, size, rating) {
-//     this.pic = pic;
-//     this.size = size;
-//     this.rating = rating;
-//     this.title = function() {
-//         if (this.rating < 5) {
-//             alert('Bad pic, not recomendet to view');
-//         }
-//         else if (this.rating == 5) {
-//             alert('Сontroversial pic, maybe good for you');
-//         }
-//         else {
-//             alert('Good pic');
-//         }
-//     };
-//     this.resolution = function() {
-//         if (size > 720) {
-//             alert('good quality');
-//         }
-//         else {
-//             alert('shit quality');
-//         }
-//     };
-// }
-
-// function Diary(task, complete, complexity) {
-//     this.task = task;
-//     this.complete = complete;
-//     this.complexity = complexity;
-//     this.date = new Date();
-//     this.is_complete = function() {
-//         if (this.complete) {
-//             alert('Task complete, good job');
-//         }
-//         else {
-//             alert('not done, hurry');
-//         }
-//     };
-//     this.difficult = function() {
-//         if (this.complexity > 5) {
-//             alert('Task is hard!');
-//         }
-//         else {
-//             alert('Task is not hard')
-//         }
-//     }
-// }
-
-
-// var cat_pic = new Image('Cat', 1080, 7);
-// var dog_pic = new Image('Doge', 720, 4);
-// var house_pic = new Image('House', 2160, 9);
-
-// cat_pic.title();
-// cat_pic.resolution();
-
-// console.log(cat_pic)
-
-
-// var task1 = new Diary('bye cock', false, 5);
-// var task2 = new Diary('bye cheese', true, 1);
-// var task3 = new Diary('complete task', false, 6);
-
-// task1.is_complete();
-// task3.difficult()
-
-// console.log(task1)
-
 window.onload = function() {
     var ingredients = {
         tomato: 20,
@@ -141,8 +73,4 @@ window.onload = function() {
     // Создание элементов
     create_ul_main('Burger1', keys_ingredients);
     create_ul_main('Burger2', keys_ingredients);
-    // create_ul_main('Burger1', burger1);
-    // create_ul_main('Burger2', burger2);
-    // create_ul_main('Salat', salat);
-    // create_ul_main('Potatoe', potatoe);
 }
